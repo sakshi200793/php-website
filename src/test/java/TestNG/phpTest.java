979e78214491b647980de88e4f0ea680c986aa62
sakshi200793 <48524439+sakshi200793@users.noreply.github.com>
2020-01-09 18:24:32 +0530

    on windows

--- src/test/java/TestNG/phpTest.java
+++ src/test/java/TestNG/phpTest.java
@@ -15,7 +15,7 @@ public class phpTest {
 	@BeforeMethod
 	public void launch()
 	{
-		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver","C:\Users\sakshikanoongo\eclipse-workspace\testproj-phpdocker\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--Headless");
 		driver = new ChromeDriver(options);
